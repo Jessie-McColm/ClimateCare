22358c58a6dd08ff4ab48ae64a6e42e098447752
Jessie McColm <jessiedragonda@gmail.com>
2023-02-15 12:27:32 +0000

    changed name of app and added templates folder

--- creature_care/creature_care/urls.py
+++ creature_care/creature_care/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('creature/',include('creature.urls')),
+    path('climate/',include('climate.urls')),
     path('admin/', admin.site.urls),
 ]
