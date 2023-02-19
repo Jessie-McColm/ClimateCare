87628743a088853cac798acba77f8a914a334a50
Jessie McColm <jessiedragonda@gmail.com>
2023-02-19 18:22:02 +0000

    started working on specifing the tests we need

--- creature_care/climate/tests.py
+++ creature_care/climate/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+def create_user(user_data,kitty_data,profile_data):
+    #set up a dummy user
+    pass
+def delete_user(userID):
+    #clean up the DB
+    pass
+
+class UserModelTests(TestCase):
+
+    def test_create_user(self):
+        #i guess test if various DB functions work?
+        self.assertIs(False, False)
+
+
+class KittyIndexTests(TestCase):
+    #class to check that the data returned from the kitty view is correct
+
+    def test_unauthorised_user(self):
+        #i guess test if various DB functions work?
+        self.assertIs(False, False)
