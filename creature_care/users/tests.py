c553eb3e29e957c32bb188e5d03b9047fdbdc4b9
Nevan Masterson <nevanmasterson1@gmail.com>
2023-02-23 14:19:17 +0000

    pair programming sesh. made a start on tests for users, not finished yet (jessie had a lecture)
    - jessie and nevan

--- creature_care/users/tests.py
+++ creature_care/users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
 
-# Create your tests here.
+class LoginViewTests(TestCase):
+
+    def test_login_user(self):
+        client = Client()
+        response = client.post(path='/users/login_user', data={
+            "username":"BabbitRabbit", "password":"password1(verysecure)"
+        }, follow=True)
+        response.
+        self.assertTrue(response.context['user'].is_authenticated)
+        # self.assertEqual(response.url, '/climate/kitty')
+        # self.assertEqual(response.status_code, 302)
+        del client
+
+    def test_invalid_username(self):
+        client = Client()
+        response = client.post(path='/users/login_user', data={
+            "username":"BabbitNotRabbit","password":"password0(verynotsecure)"
+        })
+        self.assertEqual(response.url, "/users/login_user")
+        self.assertEqual(response.status_code, 302)
+        del client
+
