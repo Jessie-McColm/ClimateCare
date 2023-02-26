f8a560cf351500dc96adbc3d8e859f7575acc825
Lucia <PigeonCoding@Outlook.com>
2023-02-26 16:22:13 +0000

    Add own decorators for permissions

---
+++ creature_care/users/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+#make decorator for not logged in user
+def unauthenticated_user(view_func):
+
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('kitty')
+        else:
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_func
--- creature_care/users/views.py
+++ creature_care/users/views.py
@@ -6,79 +6,69 @@ from climate.models import Profile, Creature
 
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
-
+from .decorators import unauthenticated_user
 
 # Create your views here.
 
-
+# only lets you access if not logged in
+@unauthenticated_user
 def register_user(request):
     # gets form I have put in forms.py which we can customise later
 
-    # if user already loggged in, redirect to the home kitty page
-    if request.user.is_authenticated:
-        return redirect('kitty')
-    # this is a test thing
+    form = CreateUserForm()
 
-    else:
-        form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            # makes user :))
+            form.save()
 
-        if request.method == "POST":
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                # makes user :))
-                form.save()
+            # can get these messages to show up on page later
+            # also way of accessing data 
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account was created for " + user)
 
-                # can get these messages to show up on page later
-                # also way of accessing data 
-                user = form.cleaned_data.get('username')
-                messages.success(request, "Account was created for " + user)
+            # -------------------
+            # creates a creature and a profile for the user 
+            user_obj = User.objects.get(username = user)
+            user_creature = Creature()
+            profile = Profile(user=user_obj, creature=user_creature)
+            user_creature.save()
+            profile.save()
+            # ------------------
 
-                # -------------------
-                # creates a creature and a profile for the user 
-                user_obj = User.objects.get(username = user)
-                user_creature = Creature()
-                profile = Profile(user=user_obj, creature=user_creature)
-                user_creature.save()
-                profile.save()
-                # ------------------
+            return redirect('loginPage')
 
-                return redirect('loginPage')
+    context = {'form':form}
+    return render(request, 'authenticate/register.html', context)
 
-        context = {'form':form}
-        return render(request, 'authenticate/register.html', context)
-
-
+@unauthenticated_user
 def login_user(request):
     # if user already logged in, redirect to the home kitty page
-    if request.user.is_authenticated:
-        return redirect('kitty')
+    # if you go to page and actually do something
+    if request.method == "POST":
+        # name passing in from the html name field
+        # username = request.POST['username'] changed to below
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # error here as need to look into the users we have and these argguments etc
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('kitty')
 
-    else:
+            # request.session['username'] = user.username
+            # return redirect('climate')
+        
+        else:
+            # will display this message in html
 
-        # if you go to page and actually do something
-        if request.method == "POST":
-            # name passing in from the html name field
-            # username = request.POST['username'] changed to below
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            # error here as need to look into the users we have and these argguments etc
-            user = authenticate(request, username=username, password=password)
-            
-            if user is not None:
-                login(request, user)
-                return redirect('kitty')
+            messages.info(request, "Username OR password is incorrect")
+            # messages.success(request, ("There was an error logging in. Please try again"))
+            return redirect('loginPage')
 
-                # request.session['username'] = user.username
-                # return redirect('climate')
-            
-            else:
-                # will display this message in html
-
-                messages.info(request, "Username OR password is incorrect")
-                # messages.success(request, ("There was an error logging in. Please try again"))
-                return redirect('loginPage')
-
-        return render(request, 'authenticate/login.html', {})
+    return render(request, 'authenticate/login.html', {})
 
 
 def logout_user(request):
